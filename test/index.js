a42b9616e27ea43bb0f78b7eed0150c116c806cc
Djuradj Ivanic <djuradj.ivanic@namics.com>
2016-11-24 13:39:36 +0100

    [tests] Add new test

--- test/index.js
+++ test/index.js
@@ -1,8 +1,13 @@
 var assert = require('assert');
 describe('Array', function () {
 	describe('#indexOf()', function () {
-		it('should return -1 when the value is not present', function() {
+		it('should return -1 when the value is not present', function () {
 			assert.equal(-1, [1, 2, 3].indexOf(4));
+		});
+	});
+	describe('#length', function () {
+		it('Should return 3 when array length is 3', function () {
+			assert.equal(3, [1, 2, 3].length);
 		});
 	});
 });
